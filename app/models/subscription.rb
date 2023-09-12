3b7b7a00e9f1baf81bf539812c4841583d9bf4f5
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-12 15:45:04 -0400

    feat: add validations to subscription model and test them

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,8 @@
 class Subscription < ApplicationRecord
-  
+  belongs_to :customer
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
+
+  validates_presence_of :title
+  validates_numericality_of :frequency
 end
---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  describe "relationships" do
+    it { should belong_to :customer }
+    it { should have_many :subscription_teas }
+    it { should have_many(:teas).through(:subscription_teas) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_numericality_of :frequency }
+  end
+end
