83c81e225db6b403dc7ff7d3c9d8f198e190736d
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-12 15:47:36 -0400

    feat: add validations to tea model and test them

---
+++ app/models/tea.rb
@@ -0,0 +1,10 @@
+class Tea < ApplicationRecord
+  has_many :subscription_teas
+  has_many :subscriptions, through: :subscription_teas
+  has_many :customers, through: :subsriptions
+
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_numericality_of :temperature
+  validates_numericality_of :brew_time
+end
