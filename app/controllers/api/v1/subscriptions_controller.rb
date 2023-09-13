b2ea703631622149453eae1ca5fd7344ac158d0a
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 15:07:29 -0400

    feat: write subscriptions_controller functionality

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,3 +1,20 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  def index; end
+  def index
+    customer = Customer.find_by_id(subscription_params[:customer_id])
+    if customer
+      if customer.subscriptions.size == 0
+        render json: {data: "Customer currently has no subscriptions."}
+      else
+        render json: SubscriptionsSerializer.new(customer.subscriptions)
+      end
+    else
+      render json: {errors: "Customer does not exist."}, status: 404
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:customer_id, :status, :id)
+  end
 end
