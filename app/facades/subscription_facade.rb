858650cb69dbce2ddd34c35331f3d0fc748589b8
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 17:28:50 -0400

    feat: make subscription_facade to handle creation of a subscription

---
+++ app/facades/subscription_facade.rb
@@ -0,0 +1,18 @@
+class SubscriptionFacade
+  def self.new_subscription(id, sub)
+    subscription = Subscription.new(customer_id: id, title: sub[:title], status: true, frequency: sub[:frequency])
+    if subscription.save
+      sub[:teas].each do |tea, quant|
+        sub_tea = SubscriptionTea.new(subscription_id: subscription.id, tea_id: tea, quantity: quant)
+        if sub_tea.save
+          next
+        else
+          return false
+        end
+      end
+    else
+      return false
+    end
+    true
+  end
+end
