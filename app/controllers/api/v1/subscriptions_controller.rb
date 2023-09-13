742602f6bab3ee18cd59eaf7422c989495a86f60
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 19:53:44 -0400

    feat: add error handling for if the teas are empty in params

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -13,6 +13,10 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def create
+    if !params[:new_subscription][:teas] || params[:new_subscription][:teas].empty?
+      render json: {errors: "Subscription cannot be empty."}, status: 422
+      return
+    end
     customer = Customer.find_by_id(subscription_params[:customer_id])
     if customer
       if SubscriptionFacade.new_subscription(customer.id, params[:new_subscription])
