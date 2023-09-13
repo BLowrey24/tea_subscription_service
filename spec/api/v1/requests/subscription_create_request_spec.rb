e2ad4039d259aa858e41b7abdb11807d71a630fc
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 17:12:33 -0400

    test: write barebones test for subscription create and add route

---
+++ spec/api/v1/requests/subscription_create_request_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe "Subscription API" do
+  before(:each) do
+    test_data
+
+    @valid_params = {
+      "new_subscription": {
+        "title": "Some stuff a LOTR's character would drink.",
+        "frequency": 5,
+        "teas": {
+          "#{@tea_1.id}": 2,
+          "#{@tea_2.id}": 4,
+          "#{@tea_3.id}": 6
+        }
+      }
+    }
+  end
+
+  describe "Happy Path" do
+    it "response is successful" do
+      post api_v1_customer_subscriptions_path(@customer_1.id), params: @valid_params
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+
+    xit "returns the data wanted" do
+      post api_v1_customer_subscriptions_path(@customer_1.id), params: @valid_params
+
+      expect(response).to be_successful
+    end
+  end
+end
