14cb24c987347a59b8ae1eba00ca81f90da80082
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 15:08:09 -0400

    feat: make subscriptions serializer

---
+++ app/serializers/subscriptions_serializer.rb
@@ -0,0 +1,10 @@
+class SubscriptionsSerializer
+  include JSONAPI::Serializer
+  attributes :customer_id, :title, :status, :frequency
+
+  attributes :price do |subscription|
+    total = subscription.subscription_teas.joins(:tea)
+      .group("subscription_teas.id")
+      .sum("subscription_teas.quantity * teas.price").values.sum
+  end
+end
--- spec/api/v1/requests/subscribe_request_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe "Subscription API" do
-  it "allows a customer to subscribe to a tea subscription" do
-    get "/api/v1/subscriptions"
-
-    expect(response).to be_successful
-  end
-end
