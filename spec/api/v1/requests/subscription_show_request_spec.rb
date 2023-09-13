333c7a7de554aecb6b0f6403676a6b583be4ddf0
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 15:07:08 -0400

    test: write happy path tests for getting a specific customers subscriptions

--- spec/api/v1/requests/subscription_show_request_spec.rb
+++ spec/api/v1/requests/subscription_show_request_spec.rb
@@ -7,10 +7,27 @@ RSpec.describe "Subscription API" do
 
   describe "index" do
     it "response to be successful" do
-      get api_v1_customer_subscriptions_path(@customer_5.id)
+      get api_v1_customer_subscriptions_path(@customer_1.id)
 
       expect(response).to be_successful
       expect(response).to have_http_status(200)
+    end
+
+    it "returns the data wanted" do
+      get api_v1_customer_subscriptions_path(@customer_1)
+
+      returned_data = JSON.parse(response.body, symbolize_names: true)
+      expect(returned_data).to have_key(:data)
+      expect(returned_data[:data].first[:type]).to eq("subscriptions")
+      expect(returned_data[:data].size).to eq(1)
+      expect(returned_data[:data].first[:id]).to eq(@subscription_1.id.to_s)
+
+      attributes = returned_data[:data].first[:attributes]
+      expect(attributes[:customer_id]).to eq(@customer_1.id)
+      expect(attributes[:title]).to be_a(String)
+      expect(attributes[:status]).to be(true).or(be(false))
+      expect(attributes[:frequency]).to be_a(Integer)
+      expect(attributes[:price]).to be_a(Float)
     end
   end
 end
