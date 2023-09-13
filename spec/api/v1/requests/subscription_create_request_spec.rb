c86a3bd3e1ba105afbb5e612b04653efc7d9b695
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 18:14:17 -0400

    test: finish test for returned data when making a subscription

--- spec/api/v1/requests/subscription_create_request_spec.rb
+++ spec/api/v1/requests/subscription_create_request_spec.rb
@@ -25,15 +25,30 @@ RSpec.describe "Subscription API" do
       expect(response).to have_http_status(200)
     end
 
-   xit "returns the data wanted" do
+   it "returns the data wanted" do
       post api_v1_customer_subscriptions_path(@customer_1.id), params: @valid_params
 
       expect(response).to be_successful
       expect(response).to have_http_status(200)
       data = JSON.parse(response.body, symbolize_names: true)
-      # require 'pry'; binding.pry
 
-      expect(data)
+      expect(data[:data][1]).to have_key(:id)
+      expect(data[:data][1][:id]).to be_a String
+      expect(data[:data][1]).to have_key(:type)
+      expect(data[:data][1][:type]).to eq("subscriptions")
+      expect(data[:data][1]).to have_key(:attributes)
+      expect(data[:data][1][:attributes]).to be_a Hash
+
+      expect(data[:data][1][:attributes]).to have_key(:customer_id)
+      expect(data[:data][1][:attributes][:customer_id]).to be_a Integer
+      expect(data[:data][1][:attributes]).to have_key(:title)
+      expect(data[:data][1][:attributes][:title]).to be_a String
+      expect(data[:data][1][:attributes]).to have_key(:status)
+      expect(data[:data][1][:attributes][:status]).to eq(true)
+      expect(data[:data][1][:attributes]).to have_key(:frequency)
+      expect(data[:data][1][:attributes][:frequency]).to be_a Integer
+      expect(data[:data][1][:attributes]).to have_key(:price)
+      expect(data[:data][1][:attributes][:price]).to be_a Float
     end
   end
 end
