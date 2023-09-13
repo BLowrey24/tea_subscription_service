27b53274f8b44474aa8bba6a6a2571d2bf08d4ab
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 15:21:21 -0400

    update: change return messages in subscriptions_controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::SubscriptionsController < ApplicationController
     customer = Customer.find_by_id(subscription_params[:customer_id])
     if customer
       if customer.subscriptions.size == 0
-        render json: {data: "Customer currently has no subscriptions."}
+        render json: {data: "This customer has no subscriptions."}
       else
         render json: SubscriptionsSerializer.new(customer.subscriptions)
       end
     else
-      render json: {errors: "Customer does not exist."}, status: 404
+      render json: {errors: "Could not find a customer with that ID."}, status: 404
     end
   end
 
