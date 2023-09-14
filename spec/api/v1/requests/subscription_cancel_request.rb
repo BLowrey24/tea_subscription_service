9c5230d8ad19ac62967dfb116d42eb106f3224d9
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 20:50:20 -0400

    test: write test for subscription cancel response and check if it actually cancels the subscription

--- spec/api/v1/requests/subscription_cancel_request.rb
+++ spec/api/v1/requests/subscription_cancel_request.rb
@@ -12,5 +12,13 @@ RSpec.describe "Subscription API" do
       expect(response).to be_successful
       expect(response).to have_http_status(204)
     end
+
+    it "changes the status correctly" do
+      patch api_v1_customer_subscription_path(@customer_1.id, @subscription_11.id), params: {status: false}
+      expect(response).to be_successful
+      expect(response).to have_http_status(204)
+
+      expect(@customer_1.subscriptions.where(status: false).count).to eq(1)
+    end
   end
 end
