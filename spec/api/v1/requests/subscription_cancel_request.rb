bc89ce09ddb47b39e44f575684530f94dcd14020
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 20:34:30 -0400

    test: write test for a successful response

---
+++ spec/api/v1/requests/subscription_cancel_request.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Subscription API" do
+  before(:each) do
+    test_data
+  end
+
+  describe "Happy Path" do
+    it "response is successful" do
+      patch api_v1_customer_subscription_path(@customer_1.id, @subscription_1.id), params: {status: false}
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(204)
+    end
+  end
+end
