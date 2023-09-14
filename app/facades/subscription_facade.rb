58824a3684fd423f1baa110f31f76eaa54b4e73f
Boston Lowrey <bostonlowrey@gmail.com>
2023-09-13 20:14:50 -0400

    feat: remove a else from subscription_facade that was not being used

--- app/facades/subscription_facade.rb
+++ app/facades/subscription_facade.rb
@@ -2,12 +2,10 @@ class SubscriptionFacade
   def self.new_subscription(id, sub)
     subscription = Subscription.new(customer_id: id, title: sub[:title], status: true, frequency: sub[:frequency])
     if subscription.save
-      sub[:teas].each do |tea, quant|
-        sub_tea = SubscriptionTea.new(subscription_id: subscription.id, tea_id: tea, quantity: quant)
+      sub[:teas].each do |tea, quan|
+        sub_tea = SubscriptionTea.new(subscription_id: subscription.id, tea_id: tea, quantity: quan)
         if sub_tea.save
           next
-        else
-          return false
         end
       end
     else
